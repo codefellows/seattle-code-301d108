c2980c181c9924d13f4c1f7a1e5c4d8dd5ab1992
JacobKnaack <jacobknck@gmail.com>
2024-03-04 12:14:03 -0800

    adds class 06 demo code

---
+++ class-06/LECTURE.md
@@ -0,0 +1,4 @@
+# Class 07 Lecture Notes
+
+* [Whiteboard Link](https://projects.invisionapp.com/freehand/document/f8357g5hG)
+* [Warm Up Repl](https://replit.com/@JacobKnaack/WarmUp06)
---
+++ class-06/demo/geolocation/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import axios from 'axios';
+import './App.css'
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+function App() {
+  const [responseData, setResponseData] = useState({});
+  const [error, setError] = useState(null);
+  const [city, setCity] = useState('');
+
+  const handleInput = (event) => {
+    let value = event.target.value;
+    setCity(value);
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getLocation(city);
+  }
+  const getLocation = async (city) => {
+    let response = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${city}&format=json`);
+    console.log(response);
+    setResponseData(response.data[0]);
+  }
+
+  const getPokemon = async () => {
+    let response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+    setResponseData(response.data);
+  }
+  const handleNext = async (url) => {
+    let response = await axios.get(url);
+    setResponseData(response.data);
+  }
+
+  console.log(responseData);
+  return (
+    <>
+      <header>
+        <h1>Geolocation</h1>
+        <form>
+          <input type="text" placeholder="Enter a city name" onChange={handleInput} />
+          <button onClick={handleSubmit}>
+            Search
+          </button>
+        </form>
+      </header>
+      <div className="card">
+        {responseData.display_name
+        ? <ol>
+            <p>{responseData.display_name}</p>
+            <img src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${responseData.lat},${responseData.lon}&zoom=9`}/>
+          </ol>
+        : <p>Please Click the button</p>
+        }
+        <button onClick={() => handleNext(responseData?.next)}>Next</button>
+      </div>
+    </>
+  )
+}
+
+export default App
