1dd5ab83741aa1cc85abd1b781f026615bb817eb
JacobKnaack <jacobknck@gmail.com>
2024-03-11 11:08:19 -0700

    adds mongoose test file

---
+++ class-11/demo/pokedex-backend/test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// modeling our Problem Domain?
+
+// figure out how to implement our idea
+// what properties or keys are being stored, what values do those map to
+let pokemonSchema = new mongoose.Schema({
+  name: String,
+  type: String,
+  health: Number,
+  attack: Number,
+});
+
+
+mongoose.connect(DATABASE_URL)
+.then(confirmation => {
+  console.log("CONNECTION ESTABLISHED!!");
+  
+  // mongoose wants to feed schemas into model constructors
+  let Pokemon = mongoose.model('pokemon', pokemonSchema);
+
+  const pikachu = new Pokemon({ name: 'Pikachu' });
+  pikachu.save();
+  console.log(pikachu);
+})
+.catch(e => {
+  console.error('CONNECTION ERROR', e);
+});
